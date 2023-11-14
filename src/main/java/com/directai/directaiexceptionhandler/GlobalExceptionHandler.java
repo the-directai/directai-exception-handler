b51d62e6a28ca1ead1f7b77c973b6116505da071
Julia Korzekwa <julia0n3@gmail.com>
2023-11-14 19:28:06 +0100

    add error codes, handling other exceptions, and logging

--- src/main/java/com/directai/directaiexceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/directai/directaiexceptionhandler/GlobalExceptionHandler.java
@@ -3,23 +3,42 @@ package com.directai.directaiexceptionhandler;
 import com.directai.directaiexceptionhandler.exception.DirectException;
 import com.directai.directaiexceptionhandler.model.ErrorModel;
 import com.directai.directaiexceptionhandler.model.ErrorModelMultipleExceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(DirectException.class)
-    ResponseEntity<ErrorModel> handleDirectException(DirectException ex){
-        if(ex.getExceptions().isEmpty()){
-            ErrorModel model = new ErrorModel(ex.getErrorCode().getErrorCode(),
-                    ex.getErrorCode().getErrorName(), ex.getDescription());
-            return new ResponseEntity<>(model, new HttpHeaders(), ex.getHttpStatus());
-        }else{
-            ErrorModelMultipleExceptions model = new ErrorModelMultipleExceptions(ex.getErrorCode().getErrorCode(),
-                    ex.getErrorCode().getErrorName(), ex.getDescription(), ex.getExceptions());
-            return new ResponseEntity<>(model, new HttpHeaders(), ex.getHttpStatus());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<ErrorModel> handleDirectException(Exception ex) {
+        log.error("Exception occurred", ex);
+        if (ex instanceof DirectException) {
+            DirectException directException = (DirectException) ex;
+            if (directException.getExceptions().isEmpty()) {
+                ErrorModel model = ErrorModel.builder()
+                        .errorCode(directException.getErrorCode().getErrorCode())
+                        .description(directException.getDescription())
+                        .errorName(directException.getErrorCode().getErrorName())
+                        .build();
+                return new ResponseEntity<>(model, new HttpHeaders(), directException.getHttpStatus());
+            } else {
+                ErrorModelMultipleExceptions model = new ErrorModelMultipleExceptions(directException.getErrorCode().getErrorCode(),
+                        directException.getErrorCode().getErrorName(), directException.getDescription(), directException.getExceptions());
+                return new ResponseEntity<>(model, new HttpHeaders(), directException.getHttpStatus());
+            }
+        } else {
+            ErrorModel model = ErrorModel.builder()
+                    .errorCode(DirectServerExceptionCode.D5000.getErrorCode())
+                    .description("An unknown exception occurred")
+                    .errorName(DirectServerExceptionCode.D5000.getErrorName())
+                    .build();
+            return new ResponseEntity<>(model, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/main/java/com/directai/directaiexceptionhandler/model/ErrorModelMultipleExceptions.java
+++ src/main/java/com/directai/directaiexceptionhandler/model/ErrorModelMultipleExceptions.java
@@ -1,5 +1,6 @@
 package com.directai.directaiexceptionhandler.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class ErrorModelMultipleExceptions extends ErrorModel {
     List<String> errors;
 
